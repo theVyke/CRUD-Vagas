d55dba8a4839cc6a5d7f78412472a7dc875b18c1
TheVyke <m.estrela777@gmail.com>
2025-05-21 13:48:41 -0300

    Atualizado index.php

--- EstacionaFACIL/index.php
+++ EstacionaFACIL/index.php
@@ -8,7 +8,7 @@ include './database/conexao.php'; // conexao com BD
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>EstacionaFácil - Gerenciamento de Vagas</title>
+  <title>EstacionaFácil</title>
   <link rel="shortcut icon" href="image/favicon.png" type="png" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
   <link rel="stylesheet" href="assets/css/style.css"/>
